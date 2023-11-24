fb0095cc97fa39d1c087171a56102447e46591a9
rmx84 <63417404+rmx84@users.noreply.github.com>
2023-11-24 14:33:01 +0100

    Add files via upload

---
+++ smiley.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstrance, HINSTANCE hPreyInstance, LPSTR lpCmdLine, int nCmdShow) 
+
+{
+        MessageBoxW(NULL, L":)", L"Smiley Alert", MB_OKCANCEL | MB_ICONINFORMATION); return 0;
+} 
